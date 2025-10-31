26413e17437d6affe4ec2e7dd6361e1827cef4db
Ibrahiim10 <ustadibrahim00@gmail.com>
2025-10-31 15:27:45 +0300

    solved production url

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["http://localhost:5173", "http://localhost:3000", , "http://localhost:dugsiye.com"]
+        origin: ["http://localhost:5173"]
     }
 ))
 
